4d76891f28bec4864c8e6aba70ab1f62e51e1383
Frederik Winther <frederikwinther@msn.com>
2018-11-21 14:48:00 +0100

    Added a user class.

---
+++ app/src/main/java/come/has/winther/puber/User.java
@@ -0,0 +1,19 @@
+package come.has.winther.puber;
+
+public class User {
+
+    private double longitude, latitude;
+    private String username;
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+}
