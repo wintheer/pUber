00f8718fa4fdb510457a8be05699474be68babf8
Frederik Winther <frederikwinther@msn.com>
2018-11-21 10:47:47 +0100

    Changed minSDK version to 16.

--- app/src/main/java/come/has/winther/puber/MapsActivity.java
+++ app/src/main/java/come/has/winther/puber/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng denmark = new LatLng(55.9397, 9.5156);
+        mMap.addMarker(new MarkerOptions().position(denmark).title("Marker in Denmark"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(denmark));
     }
 }
