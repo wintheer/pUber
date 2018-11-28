a9bcf24606d611eaea03d0e6abd066fac035e5c2
kaspersvendsen <saxSvend123>
2018-11-28 18:13:58 +0100

    Login works with Google-login and e-mail. It uses Firebase for that

--- app/src/main/java/come/has/winther/puber/LoginActivity.java
+++ app/src/main/java/come/has/winther/puber/LoginActivity.java
@@ -1,14 +1,102 @@
 package come.has.winther.puber;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LOGIN: ";
+
+    private SignInButton signInButton;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+    private TextView welcomeLabel;
+
+    private final static int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        welcomeLabel = findViewById(R.id.welcome);
+
+        signIn();
+
+    }
+
+    public void signIn(){
+        AuthUI.SignInIntentBuilder builder = AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(getSelectedProviders());
+        startActivityForResult(builder.build(), RC_SIGN_IN);
+    }
+
+
+    //start map activity when login-check reports success
+    private void startSignedInActivity(@Nullable IdpResponse response) {
+        //implement the createIntent method in MapsActivity
+        //startActivity(MapsActivity.createIntent(this, response));
+    }
+
+
+
+    //when the app opens, it check whether the user is already logged in
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startSignedInActivity(null);
+            finish();
+        }
+    }
+
+    //creates and returns a list of available providers (Google and email)
+    private List<AuthUI.IdpConfig> getSelectedProviders() {
+        List<AuthUI.IdpConfig> selectedProviders = new ArrayList<>();
+
+
+        selectedProviders.add(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        selectedProviders.add(new AuthUI.IdpConfig.EmailBuilder()
+                    .setRequireName(true)
+                    .setAllowNewAccounts(true)
+                    .build());
+
+        return selectedProviders;
     }
 }
+
+
---
+++ app/src/main/java/come/has/winther/puber/TestMapsActivity.java
@@ -0,0 +1,13 @@
+package come.has.winther.puber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestMapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_maps);
+    }
+}
